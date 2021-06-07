0c71a5c61d4e5c75042917410017d671ecad573d
sergei86 <37331072+sergei86@users.noreply.github.com>
2021-06-07 12:27:05 +0300

    Update Calc.java
    test

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -9,7 +9,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
-
+ 
  
 public class Calc {
     public static void main(String[] args) {
